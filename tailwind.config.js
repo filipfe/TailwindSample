e2af67582c2dbf38bd58f4e2e5b0577a9b5ceab0
Filip <filfer05@gmail.com>
2023-07-26 22:36:20 +0200

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4318FF",
+        p: "#707EAE",
+        font: "#1B2559",
+      },
+    },
+    fontFamily: {
+      body: ['"Plus Jakarta Sans"'],
+    },
   },
   plugins: [],
 };
